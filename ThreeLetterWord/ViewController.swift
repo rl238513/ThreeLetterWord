2f7b9af9b00968e241c3b94a0267d1bc2228947f
EDUARDO MENDOZA <rl238513@rlas-macair-36.local>
2018-11-28 11:06:13 -0600

    idk

--- ThreeLetterWord/ViewController.swift
+++ ThreeLetterWord/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var letterLable: [UILabel]!
+    @IBOutlet weak var currentLetterLable: UILabel!
+    
+    var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var counter = 0
+    var currentLetter: String!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        getAndSetCurrentLetter()
     }
 
-
+    
+    
+    
+    @IBAction func onTap(_ sender: Any) {
+        
+    }
+    func getAndSetCurrentLetter(){
+        currentLetter = letters[counter]
+        currentLetterLable.text = currentLetter
+    }
 }
 
